7005f74279ae921acc6a869788c4326bb861e353
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-07 00:59:23 +0700

    Initializing instance variables

--- html_document.py
+++ html_document.py
@@ -1,3 +1,7 @@
 class HtmlDocument:
   version = 5
   extension = "html"
+
+  def __init__(self, name, contents):
+    self.name = name
+    self.contents = contents
--- main.py
+++ main.py
@@ -3,23 +3,30 @@ from html_document import HtmlDocument
 if __name__ == "__main__":
   # Introduction to the Python instance variables
 
-  from pprint import pprint
+  # from pprint import pprint
 
-  pprint(HtmlDocument.__dict__)
-  print(type(HtmlDocument.__dict__))  # <class 'mappingproxy'>
+  # pprint(HtmlDocument.__dict__)
+  # print(type(HtmlDocument.__dict__))  # <class 'mappingproxy'>
 
-  print(HtmlDocument.version)
-  print(HtmlDocument.extension)
+  # print(HtmlDocument.version)
+  # print(HtmlDocument.extension)
 
-  html_document = HtmlDocument()
-  print(html_document.__dict__)      # {}
-  print(type(html_document.__dict__)) # <class 'dict'>
+  # html_document = HtmlDocument()
+  # print(html_document.__dict__)      # {}
+  # print(type(html_document.__dict__)) # <class 'dict'>
 
-  print(html_document.version)
-  print(html_document.extension)  # 5
+  # print(html_document.version)
+  # print(html_document.extension)  # 5
 
-  html_document.version = 6
-  print(html_document.__dict__) # {"version": 6}
-  print(html_document.version)  # 6
-  pprint(HtmlDocument.__dict__)
-  print(HtmlDocument.version)   # 5
+  # html_document.version = 6
+  # print(html_document.__dict__) # {"version": 6}
+  # print(html_document.version)  # 6
+  # pprint(HtmlDocument.__dict__)
+  # print(HtmlDocument.version)   # 5
+
+  # Initializing instance variables
+
+  blank = HtmlDocument("Blank", "")
+  print(blank.__dict__)
+  print(blank.name)
+  print(blank.contents)
